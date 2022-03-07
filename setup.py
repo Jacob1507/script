152bffbf63a81687650581d099226d54bd8bc97a
Kuba <kuba.baginski1507@gmail.com>
2022-03-07 01:14:37 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requirements=read_requirements(),
+    url="https://github.com/Jacob1507/balldontlie_script",
+    license='MIT',
     entry_points="""
         [console_scripts]
         script=script.cli:cli
